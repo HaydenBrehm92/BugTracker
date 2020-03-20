412234e5274aa7de0e2c5ed4b5a3acc475a6f6b5
haydenbrehm92@gmail.com <haydenbrehm92@gmail.com>
2020-03-20 13:24:07 -0500

    added: homepage with navbar/ buttons for partial views/ added views for creating and opening a project/ logout

--- BugTracker/Controllers/HomeController.cs
+++ BugTracker/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BugTracker.Models;
 
 namespace BugTracker.Controllers
 {
@@ -26,6 +27,33 @@ namespace BugTracker.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        // Get
+        // Home/CreateProject
+        public ActionResult CreateProject()
+        {
+            return PartialView("~/Views/Home/CreateProject.cshtml");
+        }
+
+        //Post
+        // Home/CreateProject
+        [HttpPost]
+        public ActionResult CreateProject(CreateNewProject model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model); //This needs to return the view with the partial view loaded
+            }
+
+            return View();
+        }
+
+        // Get
+        // Home/_OpenProject
+        public ActionResult OpenProject()
+        {
+            return PartialView("~/Views/Home/_OpenProject.cshtml");
         }
     }
 }
---
+++ BugTracker/Models/ProjectModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BugTracker.Models
+{
+    public class CreateNewProject
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage ="This Field is Required")]
+        [Range(1,16,ErrorMessage = "Project name must be between 1 and 16 characters")]
+        public string ProjectName { get; set; }
+
+
+    }
+}
