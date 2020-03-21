6aa087739f2fd93d306f82967d13d6d98f69ba5e
haydenbrehm92@gmail.com <haydenbrehm92@gmail.com>
2020-03-21 13:08:11 -0500

    Added: enabled migrations in Configuration.cs / added model for Project / migrated new table for Projects

--- BugTracker/Models/ProjectModel.cs
+++ BugTracker/Models/ProjectModel.cs
@@ -6,12 +6,49 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BugTracker.Models
 {
+    //possibly useless
     public class CreateNewProject
     {
-        [Required(AllowEmptyStrings = false, ErrorMessage ="This Field is Required")]
-        [Range(1,16,ErrorMessage = "Project name must be between 1 and 16 characters")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "This Field is Required")]
+        [Range(1, 16, ErrorMessage = "Project name must be between 1 and 16 characters")]
         public string ProjectName { get; set; }
 
+        public int ID { get; set; }
+        public virtual string ApplicationUserID { get; set; }
 
+    }
+
+    public class Project
+    {
+        public int ID { get; set; }
+        public string ProjectName { get; set; }
+        public virtual string ApplicationUserID { get; set; }
+        public List<BugProperties> GetBugs { get; set; }
+    }
+
+    public class BugProperties
+    {
+        public enum Category {
+            ToDo,
+            InProgress,
+            Complete
+        }
+
+        public enum Status { 
+            Severe,
+            Mild,
+            Low
+        }
+
+        public string ID { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public Category CategorySetGet { get; set; }
+
+        [Required]
+        public Status StatusSetGet { get; set; }
     }
 }
